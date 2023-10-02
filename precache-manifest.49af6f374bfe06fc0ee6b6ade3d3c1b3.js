11046d424e0fad3b143948142975748ad6bf6d60
Khushi <khuprajiv@gmail.com>
2023-10-02 14:44:52 +0530

    Updates

---
+++ precache-manifest.49af6f374bfe06fc0ee6b6ade3d3c1b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47eb9c26015a8df0e5d12ae2b3c9cacd",
+    "url": "/react-amazon-clone2/index.html"
+  },
+  {
+    "revision": "2df313082524dfce64ac",
+    "url": "/react-amazon-clone2/static/css/main.78c7c615.chunk.css"
+  },
+  {
+    "revision": "b80781e92984ad4fa754",
+    "url": "/react-amazon-clone2/static/js/2.d53695d2.chunk.js"
+  },
+  {
+    "revision": "2b587c35db5df3608932eb682dfa83dc",
+    "url": "/react-amazon-clone2/static/js/2.d53695d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2df313082524dfce64ac",
+    "url": "/react-amazon-clone2/static/js/main.26e96504.chunk.js"
+  },
+  {
+    "revision": "37d45b2dc2789f4ac67c",
+    "url": "/react-amazon-clone2/static/js/runtime-main.5d0ac3db.js"
+  }
+]);
